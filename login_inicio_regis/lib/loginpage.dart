f4344b4741e2d9fd4378fda51df4263eaba42eb7
eventuraa15 <141580602+Eduar1505@users.noreply.github.com>
2024-11-16 12:14:30 -0600

    login update

--- login_inicio_regis/lib/loginpage.dart
+++ login_inicio_regis/lib/loginpage.dart
@@ -7,7 +7,29 @@ class LoginPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.blueGrey[300],
-      
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            children: [
+
+              SizedBox(height: 125,),
+
+              Icon(Icons.person,
+              size: 100,),
+
+              SizedBox(height: 25,),
+
+              const Text(
+                'Bienvenido a Tarea Login',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 16),
+              ),
+              SizedBox(height: 10,),
+            ],
+            ),
+          ),
+        ),
     );
   }
 }
