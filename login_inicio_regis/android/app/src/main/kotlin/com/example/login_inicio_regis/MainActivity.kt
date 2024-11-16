26feb9a8c9f7709b2aa6834dbb4288a4115c6d4f
eventuraa15 <141580602+Eduar1505@users.noreply.github.com>
2024-11-16 11:58:31 -0600

    creacion de app

---
+++ login_inicio_regis/android/app/src/main/kotlin/com/example/login_inicio_regis/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.login_inicio_regis
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ login_inicio_regis/lib/loginpage.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blueGrey[300],
+      
+    );
+  }
+}
