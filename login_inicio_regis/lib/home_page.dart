92814681bed3b01fa5356deafbec8c225b37b8fe
eventuraa15 <141580602+Eduar1505@users.noreply.github.com>
2024-11-16 13:38:19 -0600

    homescreen

--- login_inicio_regis/lib/home_page.dart
+++ login_inicio_regis/lib/home_page.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key, required String email});
+  final String email;
+
+  const HomeScreen({super.key, required this.email});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(title: const Text('Pantalla de Inicio')),
+      body: Center(
+        child: Text('Bienvenido, $email', style: const TextStyle(fontSize: 18)),
+      ),
+    );
   }
 }
