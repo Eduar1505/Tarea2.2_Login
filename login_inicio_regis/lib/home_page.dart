f9648e86266699d7a8d659188ba81d073a111c5a
eventuraa15 <141580602+Eduar1505@users.noreply.github.com>
2024-11-16 13:23:47 -0600

    home page

---
+++ login_inicio_regis/lib/home_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key, required String email});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
